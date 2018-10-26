cc8168caa78f391b4f45b9e8e596c25feb93f70f
Eric Wood <ewood@squarespace.com>
2018-10-26 18:21:58 -0400

    First pass at part 1
    Waiting for Jaime to show up, so here's some code.
    Going to rewrite using a class to encapsulate the state.

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 let keywords = ["Ember", "Ruby", "Rails", "Elixir", "React"];
+
+let seenModal = false
+const emailInput = document.querySelector('input[name="email"]');
+const modal = document.querySelector('.modal');
+const yesButton = modal.querySelector('.btn-primary')
+const noButton = modal.querySelector('.btn-outline')
+
+function showModal() {
+  if (seenModal) { return }
+  modal.classList.remove('hide');
+  seenModal = true;
+}
+
+function hideModal() {
+  modal.classList.add('hide');
+}
+
+function handleInputChange() {
+  const isMatch = keywords.some((keyword) => {
+    const value = emailInput.value.toLowerCase();
+    return value.includes(keyword.toLowerCase());
+  });
+
+  if (isMatch) {
+    showModal();
+  }
+}
+
+emailInput.addEventListener('keydown', handleInputChange);
+emailInput.addEventListener('keyup', handleInputChange);
+yesButton.addEventListener('click', () => window.location = 'challenge.html');
+noButton.addEventListener('click', hideModal);
